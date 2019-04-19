7669c5e827ea843af1dedb196ff2e08f4f34998c
range244 <49761105+range244@users.noreply.github.com>
2019-04-19 11:10:51 -0600

    most of gold

---
+++ SimplePlatformer-master/SimplePlatformer/Assets/RotateObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotateSpeed;
+    public bool allowRotate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowRotate)
+
+            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
