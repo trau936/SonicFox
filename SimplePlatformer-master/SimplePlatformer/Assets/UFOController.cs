1632fb8a4b783c40318a65598c1ba74009bfdf2a
dakota <rose108@minidokaschools.org>
2019-04-25 11:01:01 -0600

    ufo

--- SimplePlatformer-master/SimplePlatformer/Assets/UFOController.cs
+++ SimplePlatformer-master/SimplePlatformer/Assets/UFOController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class UFOController : MonoBehaviour {
 
-	//Add Variables here
-
+    //Add Variables here
+    public float movementSpeed;
+    public Rigidbody2D theRB2D;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        theRB2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//Add Movement Code Here
-		
-	}
+        //Add Movement Code Here
+
+        theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))*movementSpeed;
+
+    }
 }
