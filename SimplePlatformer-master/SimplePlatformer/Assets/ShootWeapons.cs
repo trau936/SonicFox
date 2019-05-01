f0d91526eeba6e62f5f05e02cdea2683948aaa2a
range244 <49761105+range244@users.noreply.github.com>
2019-05-01 10:59:42 -0600

    Update ShootWeapons.cs

--- SimplePlatformer-master/SimplePlatformer/Assets/ShootWeapons.cs
+++ SimplePlatformer-master/SimplePlatformer/Assets/ShootWeapons.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class ShootWeapons : MonoBehaviour
 {
+    /*
     public GameObject myBullet;
+    public GameObject barrel;
+    public laserSiteControll lzerSight;
+    public float speed;
+    public float angle;
+    public UFOController myUFO;
+    public GameObject projectile;
+
     // Start is called before the first frame update
     void Start()
     {
+        myUFO = GetComponent<UFOController>();
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Instantiate(myBullet, gameObject.transform.position, gameObject.transform.rotation);
-        }
+    { ShootProjectile();
+        public void ShootProjectile()
+            //Rotate Laser
+            Vector3 dir = Input.mousePosition -
+Camera.main.WorldToScreenPoint(transform.position);
+       // if(Input.GetMouseButtonDown(0))
+       // {
+       //     Instantiate(myBullet, gameObject.transform.position, gameObject.transform.rotation);
+       // }
     }
+    */
 }
