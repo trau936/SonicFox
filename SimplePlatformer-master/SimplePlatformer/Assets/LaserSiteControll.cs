2bc6bbfea9cae0c381c91b63e0bfe54f3ac576d7
range244 <49761105+range244@users.noreply.github.com>
2019-05-07 11:01:17 -0600

    lasser

---
+++ SimplePlatformer-master/SimplePlatformer/Assets/LaserSiteControll.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSiteControll : MonoBehaviour
+{
+    public LineRenderer myLine;
+    public Vector3 startPoint;
+    public Vector3 stopPoint;
+    public Vector3[] linePoints;
+    public bool laserOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(laserOn)
+        {
+            myLine.SetPosition(0, transform.position);
+            myLine.SetPosition(1, stopPoint);
+        }
+    }
+
+    public void SetStart(Vector3 point)
+    {
+        startPoint = point;
+    }
+    public void SetEnd(Vector3 point)
+    {
+        stopPoint = point;
+    }
+
+    public void ToggleLaser()
+    {
+        laserOn = !laserOn;
+        myLine.enabled = laserOn;
+    }
+}
--- SimplePlatformer-master/SimplePlatformer/Assets/ShootWeapons.cs
+++ SimplePlatformer-master/SimplePlatformer/Assets/ShootWeapons.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ShootWeapons : MonoBehaviour
 {
-    /*
+
     public GameObject myBullet;
     public GameObject barrel;
-    public laserSiteControll lzerSight;
+    public LaserSiteControll lzerSight;
     public float speed;
     public float angle;
     public UFOController myUFO;
@@ -17,20 +17,47 @@ public class ShootWeapons : MonoBehaviour
     void Start()
     {
         myUFO = GetComponent<UFOController>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    { ShootProjectile();
-        public void ShootProjectile()
-            //Rotate Laser
-            Vector3 dir = Input.mousePosition -
-Camera.main.WorldToScreenPoint(transform.position);
-       // if(Input.GetMouseButtonDown(0))
-       // {
-       //     Instantiate(myBullet, gameObject.transform.position, gameObject.transform.rotation);
-       // }
+    {
+        ShootProjectile();
     }
-    */
+
+
+    public void ShootProjectile()
+    {
+        //Rotate Laser
+        Vector3 dir = Input.mousePosition -
+    Camera.main.WorldToScreenPoint(transform.position);
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //set laser endpoint
+        dir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        dir.z = 0f;
+        lzerSight.SetEnd(dir);
+
+        //toggle laser
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Z))
+        {
+
+            lzerSight.ToggleLaser();
+
+        }
+        //shoot projectile
+        if (Input.GetMouseButtonDown(0))
+        {
+
+
+
+            dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            Instantiate(projectile, transform.position, barrel.transform.rotation);
+        }
+    }
 }
