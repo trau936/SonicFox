c3937f78013d0293a7cb2b3936d10c7120ef6cf3
kayden <patte868@minidokaschools.org>
2019-05-10 10:46:59 -0600

    upd

--- SimplePlatformer-master/SimplePlatformer/Assets/MyLoadLevel.cs
+++ SimplePlatformer-master/SimplePlatformer/Assets/MyLoadLevel.cs
@@ -5,16 +5,24 @@ using UnityEngine.SceneManagement;
 public class MyLoadLevel : MonoBehaviour
 {
 
-
+    public string leveToLoad;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(leveToLoad);
+        }
     }
 }
